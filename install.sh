166d022cc0433d2062c7c80427e442913e47486b
root <root@JOSHUA-DESKTOP.localdomain>
2017-10-29 22:42:46 +1030

    edited defalt shell and other options in install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,24 @@
-#!/bin/zsh
-ln -s .zshrc ~/.zshrc
-ln -s .oh-my-zsh ~/.oh-my-zsh
-read -p "Clone Vim repo?" USER_INPUT
+#!/bin/bash
+read -p "Reset Config?" USER_INPUT
 if [ $USER_INPUT = "y" ] || [ $USER_INPUT = "Y" ]
 then
-    git clone --recursive "https://github.com/JoshuaGore8/vim ~/.vim"
+    rm -rf ~/.oh-my-zsh
+    rm -rf ~/.zshrc
+    rm -rf ~/.vimrc
+    rm -rf ~/.vim
+fi
+read -p "Link .zshrc and .oh-my-zsh folders? " USER_INPUT
+if [ $USER_INPUT = "y" ] || [ $USER_INPUT = "Y" ]
+then
+    ln -s $PWD/.zshrc ~/.zshrc
+    ln -s $PWD/.oh-my-zsh ~/.oh-my-zsh
+fi
+read -p "Clone Vim repo? " USER_INPUT
+if [ $USER_INPUT = "y" ] || [ $USER_INPUT = "Y" ]
+then
+    git clone --recursive "https://github.com/JoshuaGore8/vim" ~/.vim
     ln -s ~/.vim/.vimrc ~/.vimrc
+    vim -c 'PluginInstall' -c 'qa!'
     read -p "Install Prerequsites for youcompleteme?" USER_INPUT
     if [ $USER_INPUT = "y" ] || [ $USER_INPUT = "Y" ]
     then
