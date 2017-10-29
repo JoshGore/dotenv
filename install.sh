406384c17cb72890b81a7d862d62019274530e9e
root <root@JOSHUA-DESKTOP.localdomain>
2017-10-29 21:33:07 +1030

    edited if statements for prompts

--- install.sh
+++ install.sh
@@ -2,12 +2,12 @@
 ln -s .zshrc ~/.zshrc
 ln -s .oh-my-zsh ~/.oh-my-zsh
 read -p "Clone Vim repo?" USER_INPUT
-if [$USER_INPUT = "y"] or [$USER_INPUT = "Y"]
+if [ $USER_INPUT = "y" ] || [ $USER_INPUT = "Y" ]
 then
     git clone --recursive "https://github.com/JoshuaGore8/vim ~/.vim"
     ln -s ~/.vim/.vimrc ~/.vimrc
     read -p "Install Prerequsites for youcompleteme?" USER_INPUT
-    if [$USER_INPUT = "y"] or [$USER_INPUT = "Y"]
+    if [ $USER_INPUT = "y" ] || [ $USER_INPUT = "Y" ]
     then
         sudo apt-get install build-essential cmake
         sudo apt-get install python-dev python3-dev
