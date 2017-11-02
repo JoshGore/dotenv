e2df85b1b58dd5369f1912a0686a2ee6101d9356
root <root@JOSHUA-DESKTOP.localdomain>
2017-11-02 17:12:02 +1030

    Changes to be committed: 	modified:   dockerinstall.sh

--- dockerinstall.sh
+++ dockerinstall.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo 'installing some dependencies'
+sudo apt-get update && sudo apt-get install apt-transport-https ca-certificates curl software-properties-common  
 echo 'adding GPG key'
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 echo 'adding Docker repository to APT sources'
