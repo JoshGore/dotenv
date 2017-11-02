1e4e2e1a5e5a38dacf4d1a42d3c6a6246df78e4b
root <root@JOSHUA-DESKTOP.localdomain>
2017-11-02 18:05:58 +1030

    Changes to be committed: 	modified:   dockerinstall.sh

--- dockerinstall.sh
+++ dockerinstall.sh
@@ -18,6 +18,7 @@ sudo usermod -aG docker $USER
 
 echo 'installing docker-compose'
 sudo curl -L https://github.com/docker/compose/releases/download/1.17.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 echo 'checking the service is running'
 sudo systemctl status docker
