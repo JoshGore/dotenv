0d8239bf57ee3f255e9e143ffc7292563622a4ce
JoshuaGore8 <7251556+JoshuaGore8@users.noreply.github.com>
2018-08-16 15:58:59 +0930

    menu.sh added terminator and -y flags

--- menu.sh
+++ menu.sh
@@ -4,6 +4,7 @@ owd=$(pwd)
 # USER_INPUT = ""
 while [[ "$USER_INPUT" != "q" ]]
 do
+    echo "note zsh needs to be installed before config, link before changing."
     echo "Select Option, or q to quit "
     echo "1.) recursively install git submodules (if forgot on install) "
     echo "2.) delete existing config files (.oh-my-zsh, .zshrc, .vimrc, .vim "
@@ -18,18 +19,19 @@ do
     echo "11.) Install PowerLine Fonts (FontConfig Method) "
     echo "12.) Install PowerLine Fonts (Alternative Method) "
     echo "13.) Install Gnome Terminal? "
-    echo "14.) Install Docker? "
+    echo "14.) Install terminator (and prerequisites for WSL)? "
+    echo "15.) Install Docker? "
     echo "=================================================="
     read -p "Enter option # (q to quit): " USER_INPUT
-    case $USER_INPUT in 
+    case $USER_INPUT in
         1)
-            #do stuff
+            #recursively install git submodules (if forgot on install)
             git submodule update --init --recursive
             sleep 1
             clear
             ;;
         2)
-            #do stuff
+            #delete existing config files (.oh-my-zsh, .zshrc, .vimrc, .vim
             rm -rf ~/.oh-my-zsh
             rm -rf ~/.zshrc
             rm -rf ~/.vimrc
@@ -37,7 +39,7 @@ do
             clear
             ;;
         3)
-            #do stuff
+            #Link config files?
             cd ~
             ln -s $owd/.vim  ~/.vim
             ln -s $owd/.vim/.vimrc  ~/.vimrc
@@ -48,76 +50,84 @@ do
             clear
             ;;
         4)
-            #do stuff
+            #Change to zsh shell?
             echo 'trying to install zsh, in case not installed'
-            sudo apt-get update && sudo apt-get install zsh
+            sudo apt-get -y update && sudo apt-get install -y zsh
             chsh -s $(which zsh)
             clear
             ;;
         5)
-            #do stuff
+            #Install vim-plug?
             curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
                     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
             clear
             ;;
         6)
-            #do stuff
+            #Install prerequsites for YouCompleteMe?
             sudo apt-get update
             sudo apt-get install build-essential cmake
             sudo apt-get install python-dev python3-dev
             clear
             ;;
         7)
-            #do stuff
+            #Install Plugins using vim-plug?
             vim -c 'PlugClean' -c 'qa!'
             vim -c 'PlugInstall' -c 'qa!'
             clear
             ;;
         8)
-            #do stuff
+            #Install LastPass?
             chmod +x lastpass.sh
             ./lastpass.sh
             clear
             ;;
         9)
-            #do stuff
+            #Configure SSH Keys?
             chmod +x configssh.sh
             ./configssh.sh
             clear
             ;;
         10)
-            #do stuff
+            #Copy SSH Key to clipboard? (freezes on gnome terminal on windows)
             chmod +x copyssh.sh
             ./copyssh.sh
             sleep 1
             clear
             ;;
         11)
-            #do stuff
+            #Install PowerLine Fonts (FontConfig Method)
             chmod +x fontconfig-powerlineinstall.sh
             ./fontconfig-powerlineinstall.sh
             clear
             ;;
         12)
-            #do stuff
+            #Install PowerLine Fonts (Alternative Method)
             chmod +x powerlinefonts.sh
             ./powerlinefonts.sh
             clear
             ;;
         13)
-            #do stuff
+            #Install Gnome Terminal?
             echo "installing gnome-terminal"
             sudo apt-get update && sudo apt-get install gnome-terminal
             clear
             ;;
         14)
-            #do stuff
+            #Install terminator (and prerequisites for WSL)?
+            sudo apt-get install -y mesa-utils libgl1-mesa-glx dbus-x11 terminator
+            mkdir -p ~/.config
+            if [ ! -e ~/.config/terminator ] ; then
+                touch ~/.config/terminator
+            fi
+            ;;
+        15)
+            #Install Docker?
             chmod +x dockerinstall.sh
             ./dockerinstall.sh
             clear
             ;;
         [^q]*)
-            #do stuff
+            #exit script
             echo "Command not recognised, enter number or q"
             sleep 1
             clear
